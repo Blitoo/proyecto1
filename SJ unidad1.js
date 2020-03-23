c037874cbfc02aa226ebf154e1b7d575780040ef
Sora9806 <sorany.jimenez@gmail.com>
2020-03-23 11:52:34 -0300

    Agregue una funcion

--- SJ unidad1.js
+++ SJ unidad1.js
@@ -1,1 +1,10 @@
-console.log("Hola Mundo")
+console.log("Hola Mundo");
+function parImpar(num){
+    if (num %2 == 0){
+        return "Par"
+    } else {
+        return "Impar";
+    }
+}
+parImpar(3);
+console.log(parImpar(3))
